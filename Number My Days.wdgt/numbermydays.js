261b64247f15adb2832b356d58c073c954d806eb
Alistair MacDonald <amacdonald@aisquared.com>
2014-09-30 16:09:01 -0400

    First commit.

---
+++ Number My Days.wdgt/numbermydays.js
@@ -0,0 +1,27 @@
+
+
+
+  // var maleLifeExpectancyUSA = 76
+  //   , actualBirthDate       = moment('02/16/1981')
+  //   , expectedDeathDate     = moment(actualBirthDate).add('years', maleLifeExpectancyUSA)
+  //   ;
+
+  // var output = {
+  //   container  : document.getElementById('number-our-days'),
+  //   totalDays  : document.getElementById('total-days'),
+  //   longFormat : document.getElementById('long-format')
+  // };
+    
+  // setInterval(function () {
+  //   var now         = moment().valueOf()
+  //     , diff        = expectedDeathDate.valueOf() - now
+  //     , duration    = moment.duration(diff, 'milliseconds')
+  //     , daysOnEarth = expectedDeathDate.diff(now, 'days')
+  //     ;
+
+  //   output.totalDays.innerHTML = (daysOnEarth).toLocaleString() + '<sub>d</sub>';
+  //   output.longFormat.innerHTML = duration.years() + "/" + duration.months() + "/" + duration.days() + " - " +
+  //     duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
+
+  // }, 1000);
+
